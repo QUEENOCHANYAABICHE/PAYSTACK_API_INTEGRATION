55dcca10000542f2dbd93bac4e75f7a0dce75ee4
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-11 17:19:57 +0100

    update1

--- src/main/java/com/example/paystack_api/Model/PaystackPayment.java
+++ src/main/java/com/example/paystack_api/Model/PaystackPayment.java
@@ -1,4 +1,64 @@
 package com.example.paystack_api.Model;
 
+import com.example.paystack_api.enums.PricingPlanType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "paystack_payment")
 public class PaystackPayment {
-}
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+        @JoinColumn(name = "user_id")
+        private AppUser user;
+
+        @Column(name = "reference")
+        private String reference;
+
+        @Column(name = "amount")
+        private BigDecimal amount;
+
+        @Column(name = "gateway_response")
+        private String gatewayResponse;
+
+        @Column(name = "paid_at")
+        private String paidAt;
+
+        @Column(name = "created_at")
+        private String createdAt;
+
+        @Column(name = "channel")
+        private String channel;
+
+        @Column(name = "currency")
+        private String currency;
+
+        @Column(name = "ip_address")
+        private String ipAddress;
+
+        @Column(name = "pricing_plan_type", nullable = false)
+        @Enumerated(EnumType.STRING)
+        private PricingPlanType pricingPlanType = PricingPlanType.BASIC;
+
+        @CreationTimestamp
+        @Temporal(TemporalType.TIMESTAMP)
+        @Column(name = "created_on", updatable = false, nullable = false)
+        private Date createdOn;
+    }
+
+
---
+++ src/main/java/com/example/paystack_api/enums/PricingPlanType.java
@@ -0,0 +1,18 @@
+package com.example.paystack_api.enums;
+
+
+import lombok.Getter;
+
+@Getter
+public enum PricingPlanType {
+
+    BASIC("Basic"),
+    STANDARD("Standard"),
+    PREMIUM("Premium");
+
+    private final String value;
+    PricingPlanType(String value) {
+        this.value = value;
+    }
+    }
+
