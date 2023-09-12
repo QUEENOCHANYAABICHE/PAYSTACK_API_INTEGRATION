f837bcf2afbe08ab9f80a1a0fca28bc2b47d532a
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-09-12 23:12:20 +0100

    update1

--- src/main/java/com/example/paystack_api/Service/SeviceImpl/PaystackServiceImpl.java
+++ src/main/java/com/example/paystack_api/Service/SeviceImpl/PaystackServiceImpl.java
@@ -2,14 +2,29 @@ package com.example.paystack_api.Service.SeviceImpl;
 
 import com.example.paystack_api.Dto.CreatePlanDto;
 import com.example.paystack_api.Dto.InitializePaymentDto;
+import com.example.paystack_api.Repository.AppUserRepository;
+import com.example.paystack_api.Repository.PaystackPaymentRepository;
 import com.example.paystack_api.Response.CreatePlanResponse;
 import com.example.paystack_api.Response.InitializePaymentResponse;
 import com.example.paystack_api.Response.PaymentVerificationResponse;
 import com.example.paystack_api.Service.PaystackService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaystackServiceImpl implements PaystackService {
+
+    private final PaystackPaymentRepository paystackPaymentRepository;
+    private final AppUserRepository appUserRepository;
+
+    @Value("${applyforme.paystack.secret.key}")
+    private String paystackSecretKey;
+
+    public PaystackServiceImpl(PaystackPaymentRepository paystackPaymentRepository, AppUserRepository appUserRepository) {
+        this.paystackPaymentRepository = paystackPaymentRepository;
+        this.appUserRepository = appUserRepository;
+    }
+
     @Override
     public CreatePlanResponse createPlan(CreatePlanDto createPlanDto) {
         return null;
